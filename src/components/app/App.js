fa4e14332403572c48055eb49fb1b98f8db0145e
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-20 19:27:56 -0500

    Test api, create App structure

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -17,5 +17,19 @@ class App extends Component {
       .then(civs => this.setState({civs}))
       .catch(err => this.setState({ error: 'Our scouts cannot find any civs...' }))
   }
+
+  render () {
+    return(
+      <>
+        <header className="header">
+          <h1 className="title">AutoScout</h1>
+        </header>
+        <main>
+          {console.log(this.state.civs)}
+        </main>
+      </>
+    )
+  }
+
 }
 export default App;
