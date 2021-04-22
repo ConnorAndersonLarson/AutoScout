db0354b72bb7dfc7a069718c644b970bf446adf4
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-22 16:24:08 -0500

    Add crest styling, including shield image

--- src/components/civCrest/CivCrest.js
+++ src/components/civCrest/CivCrest.js
@@ -5,7 +5,8 @@ const CivCrest = ({id, exp, name, army}) => {
 
   return (
     <div className='crest' id={id} >
-      <h3 className={exp}><b>{name}</b></h3>
+      <img src="icons/shield.png" alt="grey shield being used as a background" className="crestImg" />
+      <h3 className={`${exp} civ-name`}><b>{name}</b></h3>
     </div>
   )
 }
