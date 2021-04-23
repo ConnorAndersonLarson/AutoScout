d457ead4fcd394d34e5c07b3a2ff4e6211ae4eb6
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-23 07:23:41 -0500

    Display civs by civ type

---
+++ src/components/civCrest/images.js
@@ -0,0 +1,12 @@
+import blue from './shield-blue.png';
+import green from './shield-green.png';
+import red from './shield-red.png';
+import yellow from './shield-yellow.png';
+import grey from './shield.png';
+export default  {
+  blue,
+  green,
+  red,
+  yellow,
+  grey
+}
