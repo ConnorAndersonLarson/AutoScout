bb22943c5ccb0195680abaff3b565bcc8ab934d9
Connor Anderson Larson <Connorandersonlarson@gmail.com>
2021-04-22 20:30:01 -0500

    Add styling for crests, add unique crests based on color

--- src/components/civCrest/CivCrest.js
+++ src/components/civCrest/CivCrest.js
@@ -1,11 +1,12 @@
 import React from 'react';
+//import tRoR from './shield-The Rise of Rajas.png';
 import './CivCrest.css';
 
 const CivCrest = ({id, exp, name, army}) => {
 
   return (
     <div className='crest' id={id} >
-      <img src="icons/shield.png" alt="grey shield being used as a background" className="crestImg" />
+      <img src={`icons/${exp}.png`} alt="shield being used as a background" className="crestImg" />
       <h3 className={`${exp} civ-name`}><b>{name}</b></h3>
     </div>
   )
